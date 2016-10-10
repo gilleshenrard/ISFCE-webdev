a2bb74410055ed0645396001948d6331e45340be
gilleshenrard <gilles.henrard@hotmail.com>
2016-10-10 22:56:13 +0200

    indented php code

--- cours6/liste_etudiants.php
+++ cours6/liste_etudiants.php
@@ -13,17 +13,17 @@
 
             foreach($students as $val){
                 echo "<div class='panel panel-default'>\r\n";
-                echo '<div class="panel-heading">Etudiant id='.$val["id"]."</div>\r\n";
-                echo "<div class='panel-body'>\r\n";
-                $url="\"modification_etudiant_form_envoi.php\"";
-                echo "<form method='get' action=".$url." class='form-group'>\r\n";
-                echo "<input type='text' name='id' value=\"".$val["id"]."\" hidden />\r\n";
-                echo "<label>Nom : ".$val["family_name"]."</label><br/>\r\n";
-                echo "<label>Prénom : ".$val["first_name"]."</label><br/>\r\n";
-                echo "<label>Date de naissance : ".$val["birthdate"]."</label><br/>\r\n";
-                echo "<label>Nombre de cours : ".$val["number_of_courses"]."</label><br/>\r\n";
-                echo "<button type='submit'>Modifier</button>\r\n";
-                echo "</form>\r\n";
+                    echo '<div class="panel-heading">Etudiant id='.$val["id"]."</div>\r\n";
+                    echo "<div class='panel-body'>\r\n";
+                        $url="\"modification_etudiant_form_envoi.php\"";
+                        echo "<form method='get' action=".$url." class='form-group'>\r\n";
+                            echo "<input type='text' name='id' value=\"".$val["id"]."\" hidden />\r\n";
+                            echo "<label>Nom : ".$val["family_name"]."</label><br/>\r\n";
+                            echo "<label>Prénom : ".$val["first_name"]."</label><br/>\r\n";
+                            echo "<label>Date de naissance : ".$val["birthdate"]."</label><br/>\r\n";
+                            echo "<label>Nombre de cours : ".$val["number_of_courses"]."</label><br/>\r\n";
+                            echo "<button type='submit'>Modifier</button>\r\n";
+                            echo "</form>\r\n";
                 echo "</div>\r\n</div>\r\n";
             }?>
     </body>
