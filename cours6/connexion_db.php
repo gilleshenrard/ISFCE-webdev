96b9087284941a334c9a7d3b325c8d3e7e0d3aa0
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-10-26 12:17:15 +0200

    Update connexion_db.php
    moved all code to a function
    - static member $connection to be sure that connection is initialized once only
    - *.ini file parsing and DB connection only if not done yet

--- cours6/connexion_db.php
+++ cours6/connexion_db.php
@@ -1,10 +1,16 @@
 <?php
- $dbconfig = parse_ini_file('../dbconfig.ini');
- 
- try{
-     $dbh = new PDO($dbconfig["dsn"], $dbconfig["username"], $dbconfig["password"]);
- }
- catch(PDOException $exception){
- echo "Erreur ! : " .$exception->getMessage() . "<br/>";
- die();
+function db_connect() {
+    static $connection;
+
+    try{
+        if(!isset($connection)){
+            $dbconfig = parse_ini_file('../dbconfig.ini');
+            $connection = new PDO($dbconfig["dsn"], $dbconfig["username"], $dbconfig["password"]);
+        }
+    }
+    catch(PDOException $exception){
+        echo "Erreur ! : " .$exception->getMessage() . "<br/>";
+        die();
+    }
+    return $connection;
 }
