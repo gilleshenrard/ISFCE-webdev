f53c597bf3a16965789a5ce15a71d4f040586cd7
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-10-26 12:01:07 +0200

    secured PDO creation
    replaced credentials declaration by *.ini parsing

--- cours6/connexion_db.php
+++ cours6/connexion_db.php
@@ -1,10 +1,8 @@
 <?php
- define('USER', 'DvpWeb');
- define('PASSWORD', 'ISFCE1040');
- define('DSN', 'mysql:host=localhost;dbname=school;charset=UTF8');
+ $dbconfig = parse_ini_file('../dbconfig.ini');
  
  try{
-     $dbh = new PDO(DSN, USER, PASSWORD);
+     $dbh = new PDO($dbconfig["dsn"], $dbconfig["username"], $dbconfig["password"]);
  }
  catch(PDOException $exception){
  echo "Erreur ! : " .$exception->getMessage() . "<br/>";
