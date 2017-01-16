ebf7a010820c75bf5165454c1759539841efe315
gilleshenrard <gilles.henrard@hotmail.com>
2017-01-16 20:50:05 +0100

    finish ex6

--- cours17/public_html/ex6.js
+++ cours17/public_html/ex6.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var liste = function(tab){
+    document.write("<ol>");
+    for(var i in tab)
+        document.write("<li>"+tab[i]+"</li><br/>");
 
+    document.write("</ol>");
+};
 
+var demander = function(tab){
+    for (var i=0 ; i<5 ; i++)
+        tab[i] = prompt("Entrez un ingredient");
+};
+
+tab={};
+demander(tab);
+liste(tab);
