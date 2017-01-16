942e63d6b3b28659b786e5b36b20984f850241c8
gilleshenrard <gilles.henrard@hotmail.com>
2017-01-16 19:03:04 +0100

    set a few tests up

--- cours17/public_html/index.js
+++ cours17/public_html/index.js
@@ -1,1 +1,3 @@
-var result = prompt("Quel est le cours actuel?");
+var result = prompt("Quel est le cours actuel?", "Web");
+document.write("<h1>"+result+"</h1><br/>");
+document.write(result=="Web"?"Fuck yeah!":"Fuck you!");
