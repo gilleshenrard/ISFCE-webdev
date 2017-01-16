52282ce9bad55a5d4861b7db92af1a7c0aa47265
gilleshenrard <gilles.henrard@hotmail.com>
2017-01-16 19:32:49 +0100

    actually finish ex2

--- cours17/public_html/ex2.js
+++ cours17/public_html/ex2.js
@@ -1,7 +1,19 @@
 var tab=[];
+
 tab[4]=700;
 document.write(tab.length+"<br/>");
-document.write("Premiere boucle<br/>");
-for (var i in tab){
-    document.write("L'element "+i+" vaut<br/>");
+
+for(var j=0 ; j<2 ; j++){
+    document.write("Premiere boucle<br/>");
+    for (var i in tab){
+        document.write("L'element "+i+" vaut "+tab[i]+"<br/>");
+    }
+
+    document.write("Deuxieme boucle<br/>");
+    for (var i=0 ; i<tab.length ; i++){
+        document.write("L'element "+i+" vaut "+tab[i]+"<br/>");
+    }
+
+    tab[2]=100;
+    tab[6]=300;
 }
