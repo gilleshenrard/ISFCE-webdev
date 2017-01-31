e72631feb3ba13f98072744c025a21f68c67ed9c
gilles.henrard <gilles.henrard@GHE-7.be.bvdep.net>
2017-01-31 15:52:15 +0100

    create basic project

---
+++ Henrard_PHP/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<!--
+Fait par Gilles Henrard (02/2017)
+Template Bootstrap de base récupéré sur http://getbootstrap.com/examples/jumbotron/
+Bibliothèque Bootstrap récupérée sur https://www.bootstrapcdn.com/
+-->
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Gilles Henrard</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+    </head>
+
+    <body>
+        <?php include "./vues/menu.php";?>
+        <?php include './vues/header.php';?>
+
+        <div class="container">
+            <div class="row">
+            </div>
+        </div>
+
+        <hr>
+
+        <?php include './vues/footer.php';?>
+    </body>
+</html>
