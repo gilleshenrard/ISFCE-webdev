321fb621b003c32535484436c0263ed718e3e1ac
Gilles Henrard <gilles.henrard@hotmail.com>
2017-02-01 15:23:54 +0100

    add search bar to vehicle list

--- Henrard_PHP/vues/liste_voitures.php
+++ Henrard_PHP/vues/liste_voitures.php
@@ -1,5 +1,18 @@
-<div class="list-group">
-    <a href="?page=vehicle&id=1" class="list-group-item">Car1</a>
-    <a href="?page=vehicle&id=2" class="list-group-item">Car2</a>
-    <a href="?page=vehicle&id=3" class="list-group-item">Car3</a>
+<div class="container">
+    <form method="post" action="#" class="form-inline">
+        <div class="form-group">
+            <input type="text" id="plaque" name="plaque" placeholder="Plaque à chercher" class="form-control" />
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Valider</button>
+        </div>
+    </form>
+</div>
+
+<div class="container">
+    <div class="list-group">
+        <a href="?page=vehicle&id=1" class="list-group-item">Car1</a>
+        <a href="?page=vehicle&id=2" class="list-group-item">Car2</a>
+        <a href="?page=vehicle&id=3" class="list-group-item">Car3</a>
+    </div>
 </div>
