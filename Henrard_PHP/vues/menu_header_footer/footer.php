c8ea6424ced23a0b84257722680a9fa8f97cf63d
Gilles Henrard <gilles.henrard@hotmail.com>
2017-02-01 16:24:02 +0100

    precise github url

--- Henrard_PHP/vues/menu_header_footer/footer.php
+++ Henrard_PHP/vues/menu_header_footer/footer.php
@@ -5,6 +5,6 @@
             Template Bootstrap de base : <a href="http://getbootstrap.com/examples/jumbotron/">GetBootstrap</a>.<br/>
             Bibliothèque Bootstrap : <a href="https://www.bootstrapcdn.com/">BootstrapCDN</a>.<br/>
             Petits trucs PHP : <a href="http://php.net/">PHP.net</a> et <a href="http://www.w3schools.com/">W3Schools</a><br/>
-            Dépôt GitHub du projet : <a href="https://github.com/gilleshenrard/ISFCE-webdev">GitHub</a></p>
+            Dépôt GitHub du cours de développement web : <a href="https://github.com/gilleshenrard/ISFCE-webdev">GitHub</a></p>
     </footer>
 </div>
