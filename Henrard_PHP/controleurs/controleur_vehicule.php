ffd392843686d4d8d34b261d6cf5b919bec1d8f6
Gilles Henrard <gilles.henrard@hotmail.com>
2017-02-03 09:45:35 +0100

    tried to debug cookies

--- Henrard_PHP/controleurs/controleur_vehicule.php
+++ Henrard_PHP/controleurs/controleur_vehicule.php
@@ -12,9 +12,11 @@ try{
     //connexion à la database (récupération de l'instance singleton)
     $database = new Db();
     $database->connect();
+    var_dump($_COOKIE);
 
     //Si une requête POST a été envoyée, mise à jour directe
     if (!is_null($post)) {
+        echo "<h1>BP1</h1>";
         setcookie('v_id', $id);
         setcookie('v_ch', $post['numero_chassis']);
         setcookie('v_pl', $post['plaque']);
@@ -26,13 +28,17 @@ try{
     }
     else{   //sinon, mise à jour du cookie (si pas initialisé ou si l'id a changé -> autre véhicule)
         if (is_null($cookie) or $id!=$cookie['v_id']) {
+            echo "<h1>BP2</h1>";
             $v = $database->searchBy_ID("vehicules", $id);
-            setcookie('v_id', $v['id']);
-            setcookie('v_ch', $v['numero_chassis']);
-            setcookie('v_pl', $v['plaque']);
-            setcookie('v_ma', $v['marque']);
-            setcookie('v_mo', $v['modele']);
-            setcookie('v_ty', $v['type']);
+            var_dump($id);
+            var_dump($v);
+            $_COOKIE['v_id'] = $v['id'];
+            $_COOKIE['v_ch'] = $v['numero_chassis'];
+            $_COOKIE['v_pl'] = $v['plaque'];
+            $_COOKIE['v_ma'] = $v['marque'];
+            $_COOKIE['v_mo'] = $v['modele'];
+            $_COOKIE['v_ty'] = $v['type'];
+            var_dump($_COOKIE);
         }
     }
     //Query database pour lister les réparations liées à la voiture
