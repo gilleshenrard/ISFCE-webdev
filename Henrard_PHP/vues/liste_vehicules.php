159b35425be03d127627afa6cd8f008a625f666f
Gilles Henrard <gilles.henrard@hotmail.com>
2017-02-03 17:17:12 +0100

    add spacing between search bar and list

--- Henrard_PHP/vues/liste_vehicules.php
+++ Henrard_PHP/vues/liste_vehicules.php
@@ -1,22 +1,24 @@
 <div class="container">
-    <form method="post" action="#" class="form-inline">
-        <div class="form-group">
-            <input type="text" id="plaque" name="plaque" placeholder="Plaque à chercher" class="form-control" />
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary">Chercher</button>
-        </div>
-    </form>
-</div>
+    <div class="row" style="margin-bottom: 30px;">
+        <form method="post" action="#" class="form-inline">
+            <div class="form-group">
+                <input type="text" id="plaque" name="plaque" placeholder="Plaque à chercher" class="form-control" />
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Chercher</button>
+            </div>
+        </form>
+    </div>
 
-<div class="container">
-    <div class="list-group">
-        <?php
-            foreach ($vehicles as $v) {
-                if (isset($v['plaque']) && isset($v['id'])) {
-                    echo '<a href="?page=vehicle&id='.$v['id'].'" class="list-group-item">'.$v['plaque'].'</a>';
+    <div class="row">
+        <div class="list-group">
+            <?php
+                foreach ($vehicles as $v) {
+                    if (isset($v['plaque']) && isset($v['id'])) {
+                        echo '<a href="?page=vehicle&id='.$v['id'].'" class="list-group-item">'.$v['plaque'].'</a>';
+                    }
                 }
-            }
-        ?>
+            ?>
+        </div>
     </div>
 </div>
