d9633b69a80c385e329f0addddd0d9924a1a65e5
Gilles Henrard <gilles.henrard@hotmail.com>
2017-02-03 17:17:37 +0100

    add POST testing and plate DB research

--- Henrard_PHP/controleurs/controleur_vehicules.php
+++ Henrard_PHP/controleurs/controleur_vehicules.php
@@ -1,11 +1,19 @@
 <?php
+$post = filter_input_array(INPUT_POST);
 include_once './modeles/modele_db.php';
 
 try{
     $database = new Db();
     $database->connect();
-    $vehicles =  $database->list_table("vehicules");
-    include './vues/liste_vehicules.php';
+    
+    if (!is_null($post)) {
+        $v = $database->searchBy_plaque("vehicules", $post['plaque']);
+        var_dump($v);
+    }
+    else {
+       $vehicles =  $database->list_table("vehicules");
+       include './vues/liste_vehicules.php';
+    }
 }
 catch (PDOException $e){
     echo "<h1>ERROR : ".$e->getMessage()."</h1>";
