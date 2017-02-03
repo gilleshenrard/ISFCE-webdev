949ba29dc5a5753e4632ba8aa37c0dfb01d9d248
Gilles Henrard <gilles.henrard@hotmail.com>
2017-02-03 09:55:51 +0100

    reverted to DB use (fixed conflicts)

--- Henrard_PHP/controleurs/controleur_vehicule.php
+++ Henrard_PHP/controleurs/controleur_vehicule.php
@@ -1,49 +1,28 @@
 <?php
-//utilisation de GET, POST et COOKIES sains (filtrés)
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
 $post = filter_input_array(INPUT_POST);
-
-///UTILISATION MASSIVE DES COOKIES POUR LIMITER LES APPELS A LA DB
-$cookie = filter_input_array(INPUT_COOKIE);
-
 include_once './modeles/modele_db.php';
 
 try{
-    //connexion à la database (récupération de l'instance singleton)
     $database = new Db();
     $database->connect();
-    var_dump($_COOKIE);
 
-    //Si une requête POST a été envoyée, mise à jour directe
     if (!is_null($post)) {
-        echo "<h1>BP1</h1>";
-        setcookie('v_id', $id);
-        setcookie('v_ch', $post['numero_chassis']);
-        setcookie('v_pl', $post['plaque']);
-        setcookie('v_ma', $post['marque']);
-        setcookie('v_mo', $post['modele']);
-        setcookie('v_ty', $post['type']);
-        $database->update_repa($post);
+        $v=array(
+            'id' => $id,
+            'numero_chassis' => $post['numero_chassis'],
+            'plaque' => $post['plaque'],
+            'marque' => $post['marque'],
+            'modele' => $post['modele'],
+            'type' => $post['type']
+        );
+        $database->update_repa($v);
         $_POST = array();
     }
-    else{   //sinon, mise à jour du cookie (si pas initialisé ou si l'id a changé -> autre véhicule)
-        if (is_null($cookie) or $id!=$cookie['v_id']) {
-            echo "<h1>BP2</h1>";
-            $v = $database->searchBy_ID("vehicules", $id);
-            var_dump($id);
-            var_dump($v);
-            $_COOKIE['v_id'] = $v['id'];
-            $_COOKIE['v_ch'] = $v['numero_chassis'];
-            $_COOKIE['v_pl'] = $v['plaque'];
-            $_COOKIE['v_ma'] = $v['marque'];
-            $_COOKIE['v_mo'] = $v['modele'];
-            $_COOKIE['v_ty'] = $v['type'];
-            var_dump($_COOKIE);
-        }
+    else {
+        $v = $database->searchBy_ID("vehicules", $id);
     }
-    //Query database pour lister les réparations liées à la voiture
     $rep = $database->searchBy_FK("reparations", $id);
-    //affichage
     include './vues/vehicule.php';
 }
 catch (PDOException $e){
--- Henrard_PHP/vues/vehicule.php
+++ Henrard_PHP/vues/vehicule.php
@@ -4,33 +4,33 @@
             <fieldset disabled>
                 <div class="form-group">
                     <label for="id">ID</label>
-                    <input type="text" id="id" name="id" value="<?php echo $_COOKIE['v_id'];?>" class="form-control" />
+                    <input type="text" id="id" name="id" value="<?php echo $v['id'];?>" class="form-control" />
                 </div>
             </fieldset>
 
             <div class="form-group">
                 <label for="numero_chassis">Numéro de chassis</label>
-                <input type="text" id="numero_chassis" name="numero_chassis" value="<?php echo $_COOKIE['v_ch'];?>" class="form-control" />
+                <input type="text" id="numero_chassis" name="numero_chassis" value="<?php echo $v['numero_chassis'];?>" class="form-control" />
             </div>
 
             <div class="form-group">    
                 <label for="plaque">Numéro de plaque</label>
-                <input type="text" id="plaque" name="plaque" value="<?php echo $_COOKIE['v_pl'];?>" class="form-control" />
+                <input type="text" id="plaque" name="plaque" value="<?php echo $v['plaque'];?>" class="form-control" />
             </div>
 
             <div class="form-group">    
                 <label for="marque">Marque</label>
-                <input type="text" id="marque" name="marque" value="<?php echo $_COOKIE['v_ma'];?>" class="form-control" />
+                <input type="text" id="marque" name="marque" value="<?php echo $v['marque'];?>" class="form-control" />
             </div>
 
             <div class="form-group">
                 <label for="modele">Modèle</label>
-                <input type="text" id="modele" name="modele" value="<?php echo $_COOKIE['v_mo'];?>" class="form-control" />
+                <input type="text" id="modele" name="modele" value="<?php echo $v['modele'];?>" class="form-control" />
             </div>
 
             <div class="form-group">    
                 <label for="type">Type</label>
-                <input type="text" id="type" name="type" value="<?php echo $_COOKIE['v_ty'];?>" class="form-control" />
+                <input type="text" id="type" name="type" value="<?php echo $v['type'];?>" class="form-control" />
             </div>
 
             <!--fieldset disabled-->
