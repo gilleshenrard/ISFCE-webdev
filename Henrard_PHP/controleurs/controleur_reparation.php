f477c758f640025bef0aff85e7710c763cea86e4
gilleshenrard <gilles.henrard@hotmail.com>
2017-02-19 03:44:16 +0100

    enfore sanitization

--- Henrard_PHP/controleurs/controleur_reparation.php
+++ Henrard_PHP/controleurs/controleur_reparation.php
@@ -1,31 +1,31 @@
 <?php
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
-$post = filter_input_array(INPUT_POST);
-include_once './modeles/modele_db.php';
+//include_once './controleurs/controleur_customfct.php';
 
-try{
-    $database = new Db();
-    $database->connect();
-    
-    if(!is_null($post)){
-        $r=array(
-            'id' => $id,
-            'intervention' => $post['intervention'],
-            'description' => $post['description'],
-            'date' => $post['date'],
-            'vehicule_FK' => $post['vehicule_FK']
-        );
-        $database->update_repa($r);
-        $_POST = array();
-    }
-    else {
-        $r = $database->searchBy_ID("reparations", $id);
-    }
-    include './vues/reparation.php';
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+$args = array(
+    'id' => FILTER_VALIDATE_INT,
+    'intervention' => FILTER_SANITIZE_STRING,
+    'description' => FILTER_SANITIZE_STRING,
+    'date' => FILTER_SANITIZE_STRING,
+    'vehicule_FK' => FILTER_VALIDATE_INT
+);
+$post = filter_input_array(INPUT_POST, $args);
+
+include_once './modeles/modele_db.php';
+$database = new Db();
+$database->connect();
+
+if(!is_null($post)){
+    $r=array(
+        'id' => $id,
+        'intervention' => $post['intervention'],
+        'description' => $post['description'],
+        'date' => $post['date'],
+        'vehicule_FK' => $post['vehicule_FK']
+    );
+    $database->update_repa($r);
 }
-catch (PDOException $e){
-    echo "<h1>ERROR : ".$e->getMessage()."</h1>";
+else {
+    $r = $database->searchBy_ID("reparations", $id);
 }
-catch (Exception $e){
-    echo "<h1>ERROR : ".$e->getMessage()."</h1>";
-}
+include './vues/reparation.php';
--- Henrard_PHP/controleurs/controleur_vehicule.php
+++ Henrard_PHP/controleurs/controleur_vehicule.php
@@ -2,35 +2,27 @@
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
 $post = filter_input_array(INPUT_POST);
 include_once './modeles/modele_db.php';
-include_once './controleurs/fonctions_vehicule.php';
+include_once './controleurs/controleur_customfct.php';
 
-try{
-    $database = new Db();
-    $database->connect();
+$database = new Db();
+$database->connect();
 
-    if (!is_null($post) and !isset($v)) {
-        $v=array(
-            'id' => $id,
-            'numero_chassis' => $post['numero_chassis'],
-            'plaque' => $post['plaque'],
-            'marque' => $post['marque'],
-            'modele' => $post['modele'],
-            'type' => $post['type']
-        );
-        $database->update_veh($v);
-        $_POST = array();
+if (!is_null($post) and !isset($v)) {
+    $v=array(
+        'id' => $id,
+        'numero_chassis' => $post['numero_chassis'],
+        'plaque' => $post['plaque'],
+        'marque' => $post['marque'],
+        'modele' => $post['modele'],
+        'type' => $post['type']
+    );
+    $database->update_veh($v);
+    $_POST = array();
+}
+else {
+    if (!isset($v)) {
+        $v = $database->searchBy_ID("vehicules", $id);
     }
-    else {
-        if (!isset($v)) {
-            $v = $database->searchBy_ID("vehicules", $id);
-        }
-    }
-    $rep = $database->searchBy_FK("reparations", $id);
-    include './vues/vehicule.php';
 }
-catch (PDOException $e){
-    echo "<h1>ERROR : ".$e->getMessage()."</h1>";
-}
-catch (Exception $e){
-    echo "<h1>ERROR : ".$e->getMessage()."</h1>";
-}
+$rep = $database->searchBy_FK("reparations", $id);
+include './vues/vehicule.php';
