c65bbe020b6f3e2097c1110bd1b710f1bb6f34bc
gilleshenrard <gilles.henrard@hotmail.com>
2017-02-20 22:10:37 +0100

    space code out to make it more readable

--- Henrard_PHP/controleurs/controleur_vehicule.php
+++ Henrard_PHP/controleurs/controleur_vehicule.php
@@ -3,16 +3,23 @@
 $args = array(
     'numero_chassis' => array(
         'filter' => FILTER_CALLBACK,
-        'options' => function($input){ return preg_match('#^([0-9]{5}-){3}[0-9]{5}$#',$input) ? $input : null; }),
+        'options' => function($input){
+                        return preg_match('#^([0-9]{5}-){3}[0-9]{5}$#',$input) ? $input : null; }
+        ),
     'plaque' => array(
         'filter' => FILTER_CALLBACK,
-        'options' => function($input){ return preg_match('#^(1-)?[A-Z]{3}-[0-9]{3}$#',$input) ? $input : null; }),
+        'options' => function($input){
+            return preg_match('#^(1-)?[A-Z]{3}-[0-9]{3}$#',$input) ? $input : null; }
+        ),
     'marque' => FILTER_SANITIZE_STRING,
     'modele' => FILTER_SANITIZE_STRING,
     'type' => array(
         'filter' => FILTER_CALLBACK,
-        'options' => function($input){ return in_array($input, array('Voiture','Moto','Camion','Camionette')) ? $input : null; })
+        'options' => function($input){
+            return in_array($input, array('Voiture','Moto','Camion','Camionette')) ? $input : null; }
+        )
 );
+
 //Récupération filtrée de $_POST et $_GET
 $post = filter_input_array(INPUT_POST, $args);
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
