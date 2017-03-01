d67315514f66afad46c3f021aaaf727ae4254cd8
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-01 11:51:08 +0100

    Display intervention instead of description

--- Henrard_PHP/controleurs/controleur_list_item_reparation.php
+++ Henrard_PHP/controleurs/controleur_list_item_reparation.php
@@ -1,3 +1,3 @@
 <?php
-$display = strlen($r['description'])>47 ? substr($r['description'], 0, 44)."..." : $r['description'];
+$display = strlen($r['intervention'])>47 ? substr($r['intervention'], 0, 44)."..." : $r['intervention'];
 include './vues/list_item_reparation.php';
