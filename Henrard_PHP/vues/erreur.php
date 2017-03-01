5fc58d98b12a922cefe85a799ac3e5bf1916675b
gilleshenrard <gilles.henrard@hotmail.com>
2017-03-01 21:38:31 +0100

    implement error page

---
+++ Henrard_PHP/vues/erreur.php
@@ -0,0 +1,7 @@
+<div class="container well" style="text-align: center">
+    <h1>Une erreur s'est produite</h1>
+    <p>
+        Raison :<br/>
+        <?php echo $e->getMessage();?>
+    </p>
+</div>
