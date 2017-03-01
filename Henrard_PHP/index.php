c9730b217f621ad4fc773e872d16756cb3d0c812
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-01 15:53:14 +0100

    simplify CDN url

--- Henrard_PHP/index.php
+++ Henrard_PHP/index.php
@@ -13,8 +13,9 @@ $database->connect();
     <head>
         <meta charset="utf-8" />
         <title>Gilles Henrard</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
 
     <body>
