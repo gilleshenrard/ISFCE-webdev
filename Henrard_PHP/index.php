e90dc1a18e38ea7e88d2213a23ecde36527961d7
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-01 15:00:59 +0100

    include jQuery

--- Henrard_PHP/index.php
+++ Henrard_PHP/index.php
@@ -13,6 +13,7 @@ $database->connect();
     <head>
         <meta charset="utf-8" />
         <title>Gilles Henrard</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
     </head>
 
@@ -50,4 +51,18 @@ $database->connect();
 
         include './vues/menu_header_footer/footer.php';?>
     </body>
+
+    <?php
+        switch ($page) {
+            case "vehicule":    //page d'un véhicule spécifique
+                echo "<script src='controleurs/scripts/vehicule.js' type='text/javascript'></script>";
+                break;
+
+            case "reparation":    //page d'une réparation spécifique
+                echo "<script src='controleurs/scripts/reparation.js' type='text/javascript'></script>";
+                break;
+
+            default:
+                break;
+        }?>
 </html>
