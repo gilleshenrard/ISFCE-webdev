0c6be6c4ecc4d4210d6546a06e4b543642df8bdf
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-02 14:58:46 +0100

    implement jQuery script to improve design on error

--- Henrard_PHP/vues/menu_header_footer/connexion.php
+++ Henrard_PHP/vues/menu_header_footer/connexion.php
@@ -2,11 +2,13 @@
 <div id="navbar" class="navbar-collapse collapse">
     <form method="post" action="?act=connexion" class="navbar-form navbar-right">
         <div class="form-group">
-            <input type="text" placeholder="login" class="form-control" name="login" />
+            <input type="text" placeholder="login" class="form-control" name="login" id="login" />
         </div>
       <div class="form-group">
-            <input type="password" placeholder="Password" class="form-control" name='password' />
+          <input type="password" placeholder="Password" class="form-control" name='password' id="password" />
       </div>
-      <button type="submit" class="btn btn-success">Connexion</button>
+        <button type="submit" class="btn btn-success" id="button_connexion"><span class="glyphicon glyphicon-log-in"> Connexion</span></button>
     </form>
 </div>
+
+<script type="text/javascript" src="./controleurs/scripts/connexion.js"></script>
