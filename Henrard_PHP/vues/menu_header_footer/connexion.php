552457004ab1c25475b7c73b245d365ccf6cbd28
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-02 13:24:11 +0100

    add $_GET statement for proper redirection

--- Henrard_PHP/vues/menu_header_footer/connexion.php
+++ Henrard_PHP/vues/menu_header_footer/connexion.php
@@ -1,6 +1,6 @@
 <!--Encart de connexion-->
 <div id="navbar" class="navbar-collapse collapse">
-    <form method="post" action="./" class="navbar-form navbar-right">
+    <form method="post" action="?act=connexion" class="navbar-form navbar-right">
         <div class="form-group">
             <input type="text" placeholder="login" class="form-control" name="login" />
         </div>
