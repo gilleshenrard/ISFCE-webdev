777855dffdba72c26c636c2c186a350cf7cf22e5
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-02 14:57:36 +0100

    add comments

--- Henrard_PHP/controleurs/controleur_connexion.php
+++ Henrard_PHP/controleurs/controleur_connexion.php
@@ -8,62 +8,67 @@ $opt = array(
 $post = filter_input_array(INPUT_POST, $opt);
 $act = filter_input(INPUT_GET, 'act', FILTER_SANITIZE_STRING);
 
+//Vérification qu'aucun champ n'a été laissé à vide
 if ($act=="connexion" && (is_null($post) || in_array('', $post))) {
     throw new Exception('Valeurs de connexion incorrectes');
 }
 
-//encadrement de la structure de la barre de navigation
+//création de la structure de la barre de navigation
 echo '<nav class="navbar navbar-inverse navbar-fixed-top">';
 echo '<div class="container">';
 
 //Affichage du logo principal
 include './vues/menu_header_footer/menu.php';
 
+//création de la variable d'activation/désactivation des input
 $input_disabled = "disabled";
+
 switch ($act) {
     case "connexion":       //Connexion
-        if(!isset($_SESSION) || sizeof($_SESSION, 0) <= 0) {
-            //recherche du login dans la DB et update de la session si password OK
-            $login = $database->search_user($post['login']);
-            if (sizeof($login, 0) > 0) {
-                if ($login['password'] == $post["password"]) {
-                    $_SESSION['login']=$login['login'];
-                    $input_disabled="";
-                    include './vues/menu_header_footer/greetings.php';
+            if(!isset($_SESSION) || sizeof($_SESSION, 0) <= 0) {
+                //recherche du login dans la DB et update de la session si password OK
+                $login = $database->search_user($post['login']);
+                if (sizeof($login, 0) > 0) {
+                    if ($login['password'] == $post["password"]) {
+                        $_SESSION['login']=$login['login'];
+
+                        //réactivation des inputs et affichage de l'interface de déconnexion
+                        $input_disabled="";
+                        include './vues/menu_header_footer/greetings.php';
+                    }
+                    else {
+                        throw new Exception('Mot de passe incorrect');
+                    }
                 }
                 else {
-                    throw new Exception('Mot de passe incorrect');
+                    throw new Exception("Utilisateur non-trouvé");
                 }
             }
             else {
-                throw new Exception("Utilisateur non-trouvé");
+                throw new Exception('Session déjà en cours');
             }
-        }
-        else {
-            throw new Exception('Session déjà en cours');
-        }
-        break;
+            break;
     
     case "deconnexion":     //Déconnexion de la session
-        session_unset();
-        session_destroy();
-        
-        //Affichage de l'interface de connexion
-        include './vues/menu_header_footer/connexion.php';
-        break;
+            session_unset();
+            session_destroy();
 
-    default:
-        if (isset($_SESSION) && isset($_SESSION['login'])) {
-            //Affichage de l'interface de déconnexion
-            include './vues/menu_header_footer/greetings.php';
-            $input_disabled="";
-        }
-        else {
             //Affichage de l'interface de connexion
             include './vues/menu_header_footer/connexion.php';
-            $input_disabled="disabled";
-        }
-        break;
+            break;
+
+    default:
+            if (isset($_SESSION) && isset($_SESSION['login'])) {
+                //Affichage de l'interface de déconnexion
+                include './vues/menu_header_footer/greetings.php';
+                $input_disabled="";
+            }
+            else {
+                //Affichage de l'interface de connexion
+                include './vues/menu_header_footer/connexion.php';
+                $input_disabled="disabled";
+            }
+            break;
 }
 
 //Fermeture de la barre de navigation
