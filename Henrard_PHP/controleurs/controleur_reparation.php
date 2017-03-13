d277bd2ecf559e2cc161d79119026b1f779386bf
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-13 11:02:38 +0100

    implement backend date checking

--- Henrard_PHP/controleurs/controleur_reparation.php
+++ Henrard_PHP/controleurs/controleur_reparation.php
@@ -1,22 +1,34 @@
 <?php
+/**
+ * Vérifie que le formatage et les valeurs de la date sont corrects
+ * @param string $input
+ * @return boolean
+ */
+function customCheckDate($input){
+    //Vérifie si le formatage de la date est correct
+    if(!preg_match('#^[0-9]{4}-[0-9]{2}-[0-9]{2}$#', $input)){
+        return FALSE;
+    }
+    else {
+        $inputInt = array_map('intval', explode('-', $input));
+        return check_date($inputInt[1], $inputInt[2], $inputInt[0]) ? $inputInt : FALSE;
+    }
+}
+
 //Récupération et assainissement de $_POST et $_GET
 $args = array(
     'id' => FILTER_SANITIZE_NUMBER_INT,
     'intervention' => FILTER_SANITIZE_STRING,
     'description' => FILTER_SANITIZE_STRING,
+    'vehicule_FK' => FILTER_SANITIZE_NUMBER_INT,
+    'del' => FILTER_SANITIZE_STRING,
     'date' => array(
 		'filter' => FILTER_CALLBACK,
-		'options' => function ($input){
-						return preg_match('#(19|20)[0-9]{2}-(0[0-9]|1[12])-([0-2][0-9]|3[01])#', $input) ? $input : FALSE;}
-	),
-    'vehicule_FK' => FILTER_SANITIZE_NUMBER_INT,
-    'del' => FILTER_SANITIZE_STRING
+                'options' => 'customCheckDate')
 );
 $post = filter_input_array(INPUT_POST, $args);
 $act = filter_input(INPUT_GET, "act", FILTER_SANITIZE_STRING);
 $nullerror = "La page ne doit être atteinte que via les inputs fournis";
-
-var_dump($post);
 
 //Contrôle que la page est bien accédée via une autre page (-> $_POST non-nul)
 if (is_null($post)) {
