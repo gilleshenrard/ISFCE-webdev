58c3ef3e34287e95b71e0bd279fc9c0f23b63e4d
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-13 09:26:38 +0100

    soften alphanumeric verification

--- Henrard_PHP/vues/vehicule.php
+++ Henrard_PHP/vues/vehicule.php
@@ -23,14 +23,14 @@
                 <label for="input_marque">Marque</label>
                 <input type="text" id="input_marque" name="marque" value="<?php echo $post['marque'];?>" class="form-control" placeholder="Marque" />
 				<span class="glyphicon form-control-feedback" id="fb_marque"></span>
-                <span id="tip_marque" class="help-block small hidden">Le modèle ne peut contenir que des caractères alphanumériques</span>
+                <span id="tip_marque" class="help-block small hidden">Le modèle ne peut contenir que des caractères alphanumériques et les espaces</span>
             </div>
 
             <div class="form-group has-feedback" id="group_modele">
                 <label for="input_modele">Modèle</label>
                 <input type="text" id="input_modele" name="modele" value="<?php echo $post['modele'];?>" class="form-control" placeholder="Modèle" />
 				<span class="glyphicon form-control-feedback" id="fb_modele"></span>
-                <span id="tip_modele" class="help-block small hidden">Le modèle ne peut contenir que des caractères alphanumériques</span>
+                <span id="tip_modele" class="help-block small hidden">Le modèle ne peut contenir que des caractères alphanumériques et les espaces</span>
             </div>
 
             <div class="form-group">    
