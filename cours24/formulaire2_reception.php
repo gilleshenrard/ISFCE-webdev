d02983c6ed6affc8e169b6f8c4eb397c155e859a
gilleshenrard <gilles.henrard@hotmail.com>
2017-03-13 18:59:35 +0100

    add teacher base files

---
+++ cours24/formulaire2_reception.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Reception2 - ISFCE - DvpWeb</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <script src="jquery-3.1.1.min/jquery-3.1.1.min.js"></script>
+        <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Réception des données provenant du formulaire2</h1>
+            <?php
+            var_dump($_POST);
+            // EXERCICE écrire les données en DB
+            ?>
+        </div>
+    </body>
+</html>
