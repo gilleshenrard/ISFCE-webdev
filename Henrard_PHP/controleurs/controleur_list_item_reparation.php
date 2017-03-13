980e9b5883c454fe69f00fd44a164a37da1c3a19
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-13 14:37:49 +0100

    reduce trimming length

--- Henrard_PHP/controleurs/controleur_list_item_reparation.php
+++ Henrard_PHP/controleurs/controleur_list_item_reparation.php
@@ -1,5 +1,5 @@
 <?php
 
 //Contrôle la longueur de l'intervention à afficher (max. 44 caractères)
-$display = strlen($r['intervention'])>47 ? substr($r['intervention'], 0, 44)."..." : $r['intervention'];
+$display = strlen($r['intervention'])>33 ? substr($r['intervention'], 0, 30)."..." : $r['intervention'];
 include './vues/list_items/list_item_reparation.php';
