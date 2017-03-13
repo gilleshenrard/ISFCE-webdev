fbd4a0f6bb96b5f893b53817ae3a24c3d480526d
Gilles Henrard <gilles.henrard@hotmail.com>
2017-03-13 13:49:37 +0100

    correct indentation

--- Henrard_PHP/vues/vehicule.php
+++ Henrard_PHP/vues/vehicule.php
@@ -8,28 +8,28 @@
             <div class="form-group has-feedback" id='group_chassis'>
                 <label for="input_chassis">Numéro de chassis</label>
                 <input type="text" id="input_chassis" name="numero_chassis" value="<?php echo $post['numero_chassis'];?>" class="form-control" placeholder="12345-12345-12345-12345" />
-				<span class="glyphicon form-control-feedback" id="fb_chassis"></span>
+                <span class="glyphicon form-control-feedback" id="fb_chassis"></span>
                 <span id="tip_chassis" class="help-block small hidden">Le numéro de chassis doit ressembler à : 12345-12345-12345-12345</span>
             </div>
 
             <div class="form-group has-feedback" id='group_plaque'>    
                 <label for="input_plaque">Numéro de plaque</label>
                 <input type="text" id="input_plaque" name="plaque" value="<?php echo $post['plaque'];?>" class="form-control" placeholder="(1-)ABC-123" />
-				<span class="glyphicon form-control-feedback" id="fb_plaque"></span>
+                <span class="glyphicon form-control-feedback" id="fb_plaque"></span>
                 <span id="tip_plaque" class="help-block small hidden">Le numéro de plaque doit ressembler à : (1-)ABC-123</span>
             </div>
 
             <div class="form-group has-feedback" id="group_marque">    
                 <label for="input_marque">Marque</label>
                 <input type="text" id="input_marque" name="marque" value="<?php echo $post['marque'];?>" class="form-control" placeholder="Marque" />
-				<span class="glyphicon form-control-feedback" id="fb_marque"></span>
+                <span class="glyphicon form-control-feedback" id="fb_marque"></span>
                 <span id="tip_marque" class="help-block small hidden">La marque ne peut contenir que des caractères alphanumériques et les espaces</span>
             </div>
 
             <div class="form-group has-feedback" id="group_modele">
                 <label for="input_modele">Modèle</label>
                 <input type="text" id="input_modele" name="modele" value="<?php echo $post['modele'];?>" class="form-control" placeholder="Modèle" />
-				<span class="glyphicon form-control-feedback" id="fb_modele"></span>
+                <span class="glyphicon form-control-feedback" id="fb_modele"></span>
                 <span id="tip_modele" class="help-block small hidden">Le modèle ne peut contenir que des caractères alphanumériques et les espaces</span>
             </div>
 
